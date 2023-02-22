620dfd39c968b4de3bdd21b4ead0a7ee7db96c60
sichengwen <sichengwen@kuaishou.com>
2023-02-22 13:14:53 +0800

    add

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -148,24 +148,31 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
 
     // this low-level function should be called from a contract which performs important safety checks
     function burn(address to) external lock returns (uint amount0, uint amount1) {
+        // 获取保留的tokenA、tokenB(这时不受外部合约影响的)
         (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
         address _token0 = token0;                                // gas savings
         address _token1 = token1;                                // gas savings
         uint balance0 = IERC20(_token0).balanceOf(address(this));
         uint balance1 = IERC20(_token1).balanceOf(address(this));
+        // 外部合约调用removeLiquidity时将lq token传到了该pair合约
         uint liquidity = balanceOf[address(this)];
 
         bool feeOn = _mintFee(_reserve0, _reserve1);
         uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
+        // 根据liquidity(移除的流动性代币) / totalSupply等比计算amount0、amount1
         amount0 = liquidity.mul(balance0) / _totalSupply; // using balances ensures pro-rata distribution
         amount1 = liquidity.mul(balance1) / _totalSupply; // using balances ensures pro-rata distribution
         require(amount0 > 0 && amount1 > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_BURNED');
+        // 销毁this地址的lq token代币
         _burn(address(this), liquidity);
+        // 给to发送通过lq等比计算后的token0 token1
         _safeTransfer(_token0, to, amount0);
         _safeTransfer(_token1, to, amount1);
+        // 重新计算lq池子中的tokenA tokenB的数量
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
 
+        // 更新reserve, blockTimestampLast
         _update(balance0, balance1, _reserve0, _reserve1);
         if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
         emit Burn(msg.sender, amount0, amount1, to);
